cd10190a5eb894ba900fffcda880ff370db447d1
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-09-14 20:27:36 +0200

    Add a MDN annotation

--- src/game/Renderer.ts
+++ src/game/Renderer.ts
@@ -1,6 +1,9 @@
 export default class Renderer {
   constructor(private ctx: CanvasRenderingContext2D) {}
 
+  /**
+   * From https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes#rectangular_shape_example
+   */
   testRendering() {
     this.ctx.fillRect(25, 25, 100, 100);
     this.ctx.clearRect(45, 45, 60, 60);
